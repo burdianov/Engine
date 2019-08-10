c4b583bbd53156cc73956e8f06cb3d160ce0471a
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-08-10 11:11:47 +0400

    connect to the database and run a barebone server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
 const express = require('express');
+
+// configure dotenv for environment variables access
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+// connect to the database
+const connectDB = require('./config/db');
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+// Init Middleware
+// allow using the data in req.body by applying this middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('API running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
