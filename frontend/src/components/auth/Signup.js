f482fa4a1b01e5a9da2ce2b4306933952d7fb150
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-08-11 10:25:07 +0400

    signup form and useState hook

--- frontend/src/components/auth/Signup.js
+++ frontend/src/components/auth/Signup.js
@@ -1,9 +1,116 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Signup = props => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
-    <div>
-      <h1>Sign up</h1>
+    <div className='form-wrapper'>
+      <div className='row'>
+        <div className='col-sm-10 offset-sm-2'>
+          <h1 className='h1 text-primary'>Sign Up</h1>
+          <p className='lead'>
+            <i className='fas fa-user' /> Create Your Account
+          </p>
+        </div>
+      </div>
+      <form onSubmit={onSubmit}>
+        <div className='form-group row'>
+          <label htmlFor='name' className='col-sm-2 col-form-label'>
+            Name
+          </label>
+          <div className='col-sm-10'>
+            <input
+              type='text'
+              className='form-control'
+              id='name'
+              name='name'
+              placeholder='Enter Name'
+              value={name}
+              onChange={onChange}
+              required
+            />
+            <small className='form-text text-muted'>Error messages here</small>
+          </div>
+        </div>
+        <div className='form-group row'>
+          <label htmlFor='email' className='col-sm-2 col-form-label'>
+            Email
+          </label>
+          <div className='col-sm-10'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              placeholder='Enter email'
+              required
+            />
+            <small className='form-text text-muted'>Error messages here</small>
+          </div>
+        </div>
+        <div className='form-group row'>
+          <label htmlFor='password' className='col-sm-2 col-form-label'>
+            Password
+          </label>
+          <div className='col-sm-10'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              placeholder='Password'
+              required
+            />
+          </div>
+        </div>
+        <div className='form-group row'>
+          <label htmlFor='password' className='col-sm-2 col-form-label'>
+            Confirm
+          </label>
+          <div className='col-sm-10'>
+            <input
+              type='password'
+              className='form-control'
+              id='password2'
+              name='password2'
+              value={password2}
+              onChange={onChange}
+              placeholder='Confirm Password'
+              required
+            />
+            <div>
+              <button className='btn btn-primary mt-3'>Submit</button>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
