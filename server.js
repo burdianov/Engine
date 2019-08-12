87ecb56a9939792759a2fa53f6eb66603bd4a7cc
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-08-12 20:29:23 +0400

    refactoring

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ app.use('/api/user', require('./routes/profile'));
 // serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
