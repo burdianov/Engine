46719d25ffb88f985b1982433549d234d9488840
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-08-17 19:11:19 +0400

    refactoring

--- frontend/src/components/layout/Landing.js
+++ frontend/src/components/layout/Landing.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { PropTypes } from 'prop-types';
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { PropTypes } from "prop-types";
 
 const Landing = ({ isAuthenticated }) => {
   if (isAuthenticated) {
-    return <Redirect to='/dashboard' />;
+    return <Redirect to="/dashboard" />;
   }
 
   return (
-    <section className='mt-5'>
-      <h1 className='text-center'>Engine</h1>
-      <p className='lead container'>Добро пожаловать на наш сайт!</p>
-      <div className='text-center'>
-        <Link to='/register' className='btn btn-primary'>
+    <section className="mt-5">
+      <h1 className="text-center">Enggym</h1>
+      <p className="lead container text-center">Добро пожаловать!</p>
+      <div className="text-center">
+        <Link to="/register" className="btn btn-primary">
           Sign Up
         </Link>
-        <Link to='/login' className='btn btn-success ml-2'>
+        <Link to="/login" className="btn btn-success ml-2">
           Login
         </Link>
       </div>
