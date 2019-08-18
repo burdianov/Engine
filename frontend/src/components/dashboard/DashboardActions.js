17717e7304cc7264a6f6c0f7f15bd8e2ce1b86dc
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-08-18 19:34:55 +0400

    rectify redux devtools

--- frontend/src/components/dashboard/DashboardActions.js
+++ frontend/src/components/dashboard/DashboardActions.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const DashboardActions = () => {
   return (
-    <div className='dash-buttons'>
-      <Link to='/edit-profile' className='btn btn-light'>
-        <i className='fas fa-user-circle text-primary' /> Edit Profile
+    <div className="dash-buttons">
+      <Link to="/edit-profile" className="btn btn-light">
+        <i className="fas fa-user-circle text-primary" /> Edit Profile
       </Link>
-      <Link to='/classroom' className='btn btn-light'>
-        <i className='fas fa-chalkboard-teacher text-primary' /> Got to
-        Classroom
+      <Link to="/classroom" className="btn btn-light">
+        <i className="fas fa-chalkboard-teacher text-primary" /> Go to Classroom
       </Link>
-      <Link to='/games' className='btn btn-light'>
-        <i className='fas fa-gamepad text-primary' /> Go to Games
+      <Link to="/games" className="btn btn-light">
+        <i className="fas fa-gamepad text-primary" /> Go to Games
       </Link>
     </div>
   );
